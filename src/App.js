390452a80713943bc2015f850690d1f98b409864
unknown <gonzalezcastroivanpatricio@gmail.com>
2025-01-19 20:12:13 -0500

    Practicum 1.2

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Inicio from './Inicio'; 
+import Historia from './SomosFunerariaJaramillo/Historia';
+import Objetivos from './SomosFunerariaJaramillo/Objetivos';
+import DepartamentosMision from './SomosFunerariaJaramillo/DepartamentosMision';
+import Organigrama from './SomosFunerariaJaramillo/Organigrama';
+import Productos from './SomosFunerariaJaramillo/Productos';
+import Ofertas from './Cursos/Ofertas';
+import Home from './Cursos/Home';
+import Explorar from './Cursos/Explorar';
+import Quiz from './Cursos/Quiz';
+import IrCurso from './Cursos/IrCurso';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/SomosFunerariaJaramillo/Historia" element={<Historia />} />
+          <Route path="/SomosFunerariaJaramillo/Objetivos" element={<Objetivos />} />
+          <Route path="/SomosFunerariaJaramillo/DepartamentosMision" element={<DepartamentosMision />} />
+          <Route path="/SomosFunerariaJaramillo/Organigrama" element={<Organigrama />} />
+          <Route path="/SomosFunerariaJaramillo/Productos" element={<Productos />} />
+          <Route path="/Cursos/Ofertas" element={<Ofertas />} /> 
+          <Route path="/Cursos/Home" element={<Home />} /> 
+          <Route path="/Cursos/Explorar" element={<Explorar />} /> 
+          <Route path="/Cursos/Quiz" element={<Quiz />} /> 
+          <Route path="/Cursos/IrCurso" element={<IrCurso />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
